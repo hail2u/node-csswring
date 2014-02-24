4e8086bb0886a76aac0bad34b1fd299c73b6ae4b
Kyo Nagashima <kyo@hail2u.net>
2014-02-24 22:45:50 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+CSSWring
+========
+
+Minify CSS file. That's only.
+
+Written with [PostCSS][1].
+
+
+INSTALLATION
+------------
+
+    $ npm install csswring
+
+
+QUICK USAGE
+-----------
+
+    #!/usr/bin/env node
+    
+    'use strict';
+    
+    var fs = require('fs');
+    var csswring = require('csswring');
+    
+    var css = fs.readFileSync('test.css', {
+      encoding: 'utf8'
+    });
+    fs.writeFileSync('test.min.css', csswring.wring(css));
+
+
+LICENSE
+-------
+
+MIT: http://hail2u.mit-license.org/2014
+
+
+[1]: https://github.com/ai/postcss
---
+++ test/test.js
@@ -0,0 +1,61 @@
+/*jshint node:true */
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var postcss = require('postcss');
+
+var csswring = require('../index');
+
+var fixtures = path.join(__dirname, 'fixtures');
+var input = '';
+var expected = '';
+var opts = {};
+var _loadInput = function (name) {
+  return fs.readFileSync(path.join(fixtures, name + '-input.css'), {
+    encoding: 'utf8'
+  });
+};
+var _loadExpected = function (name) {
+  return fs.readFileSync(path.join(fixtures, name + '-expected.css'), {
+    encoding: 'utf8'
+  });
+};
+
+exports.testPublicInterfaces = function (test) {
+  test.expect(3);
+
+  input = '.foo{color:black}';
+  expected = postcss.parse(input);
+  test.strictEqual(csswring.wring(input).css, expected.toString());
+
+  opts.map = true;
+  test.strictEqual(
+    csswring.wring(input, opts).map,
+    expected.toResult(opts).map
+  );
+
+  test.strictEqual(postcss().use(csswring.processor).process(input).css, expected.toString());
+
+  test.done();
+};
+
+exports.testRealCSS = function (test) {
+  test.expect(4);
+
+  var testCases = [
+    'simple',
+    'extra-semicolons',
+    'empty-declarations',
+    'single-charset'
+  ];
+
+  for (var i = 0, l = testCases.length; i < l; i++) {
+    var testCase = testCases[i];
+    input = _loadInput(testCase);
+    expected = _loadExpected(testCase);
+    test.strictEqual(csswring.wring(input).css, expected);
+  }
+
+  test.done();
+};
