5ca60ec0a470d86e6dc0cbaa2440dbd0ba126270
Kyo Nagashima <kyo@hail2u.net>
2014-06-29 16:54:40 +0900

    Format code

--- test/csswring_test.js
+++ test/csswring_test.js
@@ -26,11 +26,18 @@ exports.testPublicInterfaces = function (test) {
   expected = postcss.parse(input);
 
   // csswring.wring()
-  test.strictEqual(csswring.wring(input).css, expected.toString());
+  test.strictEqual(
+    csswring.wring(input).css,
+    expected.toString()
+  );
 
   // csswring().wring()
-  test.strictEqual(csswring().wring(input).css, expected.toString());
+  test.strictEqual(
+    csswring().wring(input).css,
+    expected.toString()
+  );
 
+  // csswring.wring({ map: true })
   opts.map = true;
   test.deepEqual(
     csswring.wring(input, opts).map,
@@ -62,60 +69,65 @@ exports.testPublicInterfaces = function (test) {
 exports.testOptions = function (test) {
   test.expect(7);
 
-  input = '.foo{color:black}';
-  expected = postcss.parse(input);
+  var testCase= '';
 
-  // preserveHacks
+  // csswring.preserveHacks
   csswring.preserveHacks = true;
-  var testCase = 'preserve-hacks';
+  testCase = 'preserve-hacks';
   var preserveHacksInput = loadInput(testCase);
   var preserveHacksExpected = loadExpected(testCase);
-  test.strictEqual(csswring.wring(preserveHacksInput).css, preserveHacksExpected);
+  test.strictEqual(
+    csswring.wring(preserveHacksInput).css,
+    preserveHacksExpected
+  );
   csswring.preserveHacks = false;
 
-  // removeAllComments
+  // csswring.removeAllComments
   csswring.removeAllComments = true;
   opts.map = true;
   testCase = 'remove-all-comments';
   var removeAllCommentsInput = loadInput(testCase);
   var removeAllCommentsExpected = loadExpected(testCase);
-  test.strictEqual(csswring.wring(removeAllCommentsInput, opts).css, removeAllCommentsExpected);
+  test.strictEqual(
+    csswring.wring(removeAllCommentsInput, opts).css,
+    removeAllCommentsExpected
+  );
   csswring.removeAllComments = false;
   opts.map = undefined;
 
   // csswring.wring(css, options)
-  test.strictEqual(csswring.wring(preserveHacksInput, {preserveHacks: true}).css, preserveHacksExpected);
+  test.strictEqual(
+    csswring.wring(preserveHacksInput, {
+      preserveHacks: true
+    }).css,
+    preserveHacksExpected
+  );
 
   // csswring(options).wring()
-  test.strictEqual(csswring({preserveHacks: true}).wring(preserveHacksInput).css, preserveHacksExpected);
+  test.strictEqual(
+    csswring({
+      preserveHacks: true
+    }).wring(preserveHacksInput).css,
+    preserveHacksExpected
+  );
 
   // csswring(options).postcss
   test.strictEqual(
-    postcss()
-      .use(csswring({preserveHacks: true}).postcss)
-      .process(preserveHacksInput)
-      .css,
+    postcss().use(csswring({
+      preserveHacks: true
+    }).postcss).process(preserveHacksInput).css,
     preserveHacksExpected
   );
 
-  // instances do not share settings
-  var a = csswring({preserveHacks: true});
+  // options per instance
+  var a = csswring({
+    preserveHacks: true
+  });
   var b = csswring();
 
-  test.strictEqual(
-    postcss()
-      .use(a.postcss)
-      .process(preserveHacksInput)
-      .css,
-    preserveHacksExpected
-  );
-
-  test.strictEqual(
-    postcss()
-      .use(b.postcss)
-      .process(input)
-      .css,
-    expected.toString()
+  test.notStrictEqual(
+    postcss().use(a.postcss).process(preserveHacksInput).css,
+    postcss().use(b.postcss).process(preserveHacksInput).css
   );
 
   test.done();
@@ -137,7 +149,10 @@ exports.testRealCSS = function (test) {
     var testCase = testCases[i];
     input = loadInput(testCase);
     expected = loadExpected(testCase);
-    test.strictEqual(csswring.wring(input).css, expected);
+    test.strictEqual(
+      csswring.wring(input).css,
+      expected
+    );
   }
 
   test.done();
